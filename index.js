ca803ecb8d4f1e41c8a983944acc392356075149
Ashures <Ashures>
2023-09-13 15:33:02 +1200

    Replaced typescript with javascript

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import root from './routers/index';
 import about from './routers/about';
 
 const app = express();
-const port: number = 3000;
+const port = 3000;
 
 app.use('/', root);
 app.use('/about/', about);
